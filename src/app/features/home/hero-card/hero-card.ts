7e45dcaab9e8a25620bcd6343837521d77e5260f
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2025-07-22 13:36:44 -0400

    Creating card component in home

---
+++ src/app/features/home/hero-card/hero-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-card',
+  imports: [],
+  templateUrl: './hero-card.html',
+  styleUrl: './hero-card.css',
+})
+export class HeroCard {
+  message = 'Test';
+}
--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,9 +1,10 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeroCard } from './hero-card/hero-card';
 
 @Component({
   selector: 'app-home',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, HeroCard],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
